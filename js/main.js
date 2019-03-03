cac870aadd145be56d8011ea5ef2199496c2dff7
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2019-03-03 00:39:05 -0300

    Adiciona rotina de cliques nas especialidades
    Os detalhes ainda não estão presentes.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-// JavaScript source code
+$(".tab2").mouseenter(function() {
+    $(this).addClass("red-link");
+
+    var div = $(".dot-area");
+    div.append('<div class="red-dot"></div>');
+    div.css("top", $(this).position().top + 7);
+});
+
+$(".tab2").mouseleave(function() {
+    $(this).removeClass("red-link");
+
+    var div = $(".dot-area");
+    div.empty();
+});
+
+$(".tab2").click(function() {
+    $(".code-area > .tab2").removeClass("yellow-link");
+    $(this).addClass("yellow-link");
+
+    var div = $(".dot-area");
+    div.append('<div class="yellow-dot"></div>');
+    div.css("top", $(this).position().top + 7);
+});
